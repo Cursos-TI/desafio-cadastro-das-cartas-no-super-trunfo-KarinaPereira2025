a8721d8f2100f54bd73bb0e1a9b7d171ba4353d4
KarinaPereira2025 <karinaff.pereira@gmail.com>
2025-06-12 13:55:10 +0000

    Comei d codigo de novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,49 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+//Desafio Super Trunfo Paises
+//Cadastro de Cartas
 
-int main() {
-    printf("Desafio Cartas  Super  Trunfo\n");
+int main(){
+
+    //Declaracao de variaveis para carta 1
+
+    char Estado1 = 'C'; 
+    char Codigo_carta1[3] = "C01";
+    char Nome_cidade1[20] = "Belem";
+    int Populacao1 = 1520;
+    float Area1 = 2250;
+    float Pib1 = 25444;
+   
+
+    //Declaracao de varieves para carta 2
+
+    char Estado2 = 'B';
+    char  Codigo_carta2[3] = "B02";
+    char  Nome_cidade2[20] = "Fortaleza";
+    int  Populacao2 = 2500;
+    float Area2 = 3255;
+    float Pib2 = 4577;
+   
+
+    //Insercao de  dados na carta 1
+
+    printf("Digite uma letra para Estado da carta1:");
+    scanf("%c", &Estado1);
+    scanf( "%c", &Estado1);
+
+    printf("Digite o codigo da carta1:");
+    scanf("%c", &Codigo_carta1);
+    scanf( "%c", &Codigo_carta1);
 
 
-    int Carta = 1;
-    char Estado = 'A';
-    char Codigo = 'A01';
-    char Cidade[20] = "Manaus";
-    int Populacao = 500;
-    float Area =  160;
-    float Pib = 220;
-    int PontoTuristico = 50;
-
-    printf("Carta: %d \n", Carta);
-    printf("Estado: %c\n", Estado);
-    printf("Codigo: %c\n",Codigo);
-    printf("Cidade: %s\n", Cidade);
-    printf("Populacao: %d\n", Populacao);
-    printf("Area: %f\n", Area);
-    printf("Pib: %f\n",Pib);int carta = 1;
-    printf("PontoTuristico:%d\n",PontoTuristico);
 
 
-    int Carta =  2;
-    char Estado = 'C';
-    char Codigo = 'A02';
-    char Cidade[20] = "Ceara";
-    int Populacao = 700;
-    float Area = 300;
-    float Pib = 315;
-    int PontoTuristico = 40;
-
-
-    printf("Carta: %d \n", Carta);
-    printf("Estado: %c\n", Estado);
-    printf("Codigo: %c\n",Codigo);
-    printf("Cidade: %s\n", Cidade);
-    printf("Populacao: %d\n", Populacao);
-    printf("Area: %f\n", Area);
-    printf("Pib: %f\n",Pib);int carta = 1;
-    printf("PontoTuristico:%d\n",PontoTuristico);
-        
 }
-
     
