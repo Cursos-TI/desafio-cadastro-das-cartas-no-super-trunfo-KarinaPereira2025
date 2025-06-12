8ed367fd540e1c1eab81f1b1486487adba85649a
KarinaPereira2025 <karinaff.pereira@gmail.com>
2025-06-12 02:33:59 +0000

    adicionei carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,19 +4,46 @@
 
 int main() {
     printf("Desafio Cartas  Super  Trunfo\n");
-    int carta = 1;
-    char Estado = "Amazonas";
-    char Codigo =  "A01";
-    char Cidade[20] =  "Manaus";
-    
 
-    printf("Carta: %d \n",  carta);
-    printf("Estado: %s\n","Amazonas");
-    printf("Codigo: %s\n","A01");
-    printf("Cidade: %s\n","Manaus");
-    
-    
-    
+
+    int Carta = 1;
+    char Estado = 'A';
+    char Codigo = 'A01';
+    char Cidade[20] = "Manaus";
+    int Populacao = 500;
+    float Area =  160;
+    float Pib = 220;
+    int PontoTuristico = 50;
+
+    printf("Carta: %d \n", Carta);
+    printf("Estado: %c\n", Estado);
+    printf("Codigo: %c\n",Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Populacao: %d\n", Populacao);
+    printf("Area: %f\n", Area);
+    printf("Pib: %f\n",Pib);int carta = 1;
+    printf("PontoTuristico:%d\n",PontoTuristico);
+
+
+    int Carta =  2;
+    char Estado = 'C';
+    char Codigo = 'A02';
+    char Cidade[20] = "Ceara";
+    int Populacao = 700;
+    float Area = 300;
+    float Pib = 315;
+    int PontoTuristico = 40;
+
+
+    printf("Carta: %d \n", Carta);
+    printf("Estado: %c\n", Estado);
+    printf("Codigo: %c\n",Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Populacao: %d\n", Populacao);
+    printf("Area: %f\n", Area);
+    printf("Pib: %f\n",Pib);int carta = 1;
+    printf("PontoTuristico:%d\n",PontoTuristico);
         
-    return 0;
 }
+
+    
