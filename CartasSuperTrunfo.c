2944efe6da543b90cf84eac8b26ede8c45ba712a
KarinaPereira2025 <karinaff.pereira@gmail.com>
2025-06-17 13:51:58 +0000

    Nova edicao do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,35 +8,28 @@ int main(){
     //Declaracao de variaveis para carta 1
 
     char Estado1 = 'C'; 
-    char Codigo_carta1[3] = "C01";
-    char Nome_cidade1[20] = "Belem";
-    int Populacao1 = 1520;
+    char Codigo_carta1[10] = "C01";
+    char Cidade1[32] = "Belem";
+    int Populacao1 = 150000;
     float Area1 = 2250;
     float Pib1 = 25444;
-   
+    int Pontoturistico1 = 50;
+  
 
-    //Declaracao de varieves para carta 2
+    printf(" Dados da Carta 1\n");
+    printf("Estado: %c\n", Estado1);
+    printf("Codigo da  Carta: %s\n", Codigo_carta1);
+    printf("Nome da Cidade:%S\n", Cidade1);
+    printf("Populacao: %d\n", Populacao1);
+    printf("Area: %2f\n", Area1);
+    printf("PIB: %2f\n", Pib1);
+    printf("Pontos Turisticos: %d\n", Pontoturistico1);
 
-    char Estado2 = 'B';
-    char  Codigo_carta2[3] = "B02";
-    char  Nome_cidade2[20] = "Fortaleza";
-    int  Populacao2 = 2500;
-    float Area2 = 3255;
-    float Pib2 = 4577;
-   
-
-    //Insercao de  dados na carta 1
-
-    printf("Digite uma letra para Estado da carta1:");
-    scanf("%c", &Estado1);
-    scanf( "%c", &Estado1);
-
-    printf("Digite o codigo da carta1:");
-    scanf("%c", &Codigo_carta1);
-    scanf( "%c", &Codigo_carta1);
-
-
-
-
+    
 }
+
+
+
+
+
     
